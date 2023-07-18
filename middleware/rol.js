4127c9bf9955b4e0cbdde0d9718a832e3223382d
Paula <Paula_reiche@hotmail.com>
2023-07-18 17:02:47 -0300

    Permisos con roles

---
+++ middleware/rol.js
@@ -0,0 +1,20 @@
+const { handleHttpError } = require('../utils/handleError')
+
+const checkRol = (roles) => (req, res, next) => {
+    try{
+        const { user } = req
+        const rolesByUser = user.role
+        const checkValueRol = roles.some((rolSingle) => rolesByUser.includes(rolSingle))
+
+        if(!checkValueRol){
+            handleHttpError(res, 'ERROR_NOT_PERMISSIONS', 403)
+            return
+        }
+        next()
+    }catch(e){
+        handleHttpError(res, 'ERROR_ROLE', 403)
+    }
+}
+
+
+module.exports = checkRol
--- routes/partituras.js
+++ routes/partituras.js
@@ -1,17 +1,19 @@
 const express = require('express')
 const { getItems, getItem, createItem, updateItem, deleteItem } = require('../controllers/partituras')
+const authMiddleware = require('../middleware/session')
+const checkRol = require('../middleware/rol')
 const { validatorCreateItem, validatorGetItem } = require('../validators/partituras')
 const router = express.Router()
 
-router.get('/', getItems)
+router.get('/', authMiddleware, getItems)
 
-router.get('/:id', validatorGetItem, getItem)
+router.get('/:id', authMiddleware, validatorGetItem, getItem)
 
-router.post('/', validatorCreateItem, createItem)
+router.post('/', authMiddleware, checkRol(['admin']), validatorCreateItem, createItem)
 
-router.put('/:id', validatorCreateItem, validatorGetItem, updateItem)
+router.put('/:id', authMiddleware, validatorCreateItem, validatorGetItem, updateItem)
 
-router.delete('/:id', validatorGetItem, deleteItem)
+router.delete('/:id', authMiddleware, validatorGetItem, deleteItem)
 
 
 module.exports = router
